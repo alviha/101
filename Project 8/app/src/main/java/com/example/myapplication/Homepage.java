043b126c1f83e17df2bf7e16f47017fee78281b7
Joey Luck <Luckjd@vcu.edu>
2019-03-10 17:04:30 -0400

    Finished layout for homepage, added the rest of the IDs

--- Project 8/app/src/main/java/com/example/myapplication/Homepage.java
+++ Project 8/app/src/main/java/com/example/myapplication/Homepage.java
@@ -1,4 +1,14 @@
 package com.example.myapplication;
 
-public class Homepage {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
 }
+
