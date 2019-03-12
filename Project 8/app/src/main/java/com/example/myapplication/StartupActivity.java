ad9dc92051dbc98fb71e85f0dadcbc99c67e8d15
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-03-11 21:43:20 -0400

    remove notification bar on startup

--- Project 8/app/src/main/java/com/example/myapplication/StartupActivity.java
+++ Project 8/app/src/main/java/com/example/myapplication/StartupActivity.java
@@ -4,12 +4,19 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowId;
+import android.view.WindowManager;
 
 public class StartupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /** Remove notification bar */
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_startup);
 
         splashScreen();
@@ -17,7 +24,7 @@ public class StartupActivity extends AppCompatActivity {
 
     private void splashScreen() {
 
-        long timeDelay = 3000;
+        long timeDelay = 3000; // 3 second delay
         new Handler().postDelayed(new Runnable() {
 
             @Override
