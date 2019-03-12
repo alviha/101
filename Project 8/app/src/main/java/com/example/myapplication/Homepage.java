4bd3402969fb065202c5ac09c5c94f18a1ef39e3
Joey Luck <Luckjd@vcu.edu>
2019-03-11 22:27:33 -0400

    navigation bar added, homepage java code added

--- Project 8/app/src/main/java/com/example/myapplication/Homepage.java
+++ Project 8/app/src/main/java/com/example/myapplication/Homepage.java
@@ -1,14 +1,18 @@
 package com.example.myapplication;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class Homepage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+
+    }
 }
 
