47adb33b82d1b0b68f15f1ab32b5d49bbeb9478f
Hassan Alvi <alviha@mymail.vcu.edu>
2019-03-15 18:38:05 -0400

    Commit

--- Project 8/app/src/main/java/com/example/myapplication/ForgotEmailActivity.java
+++ Project 8/app/src/main/java/com/example/myapplication/ForgotEmailActivity.java
@@ -2,13 +2,36 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class ForgotEmailActivity extends AppCompatActivity {
+public class ForgotEmailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText secondaryEmail;
+    private EditText username;
+
+    private Button sendEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_email);
+
+        secondaryEmail = findViewById(R.id.input_Email);
+        username = findViewById(R.id.input_username);
+
+        sendEmail = findViewById(R.id.button_SendEmail);
+        sendEmail.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == sendEmail){
+
+        }
 
     }
 }
