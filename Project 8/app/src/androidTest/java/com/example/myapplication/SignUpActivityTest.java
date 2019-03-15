a74a64e29cf94a5ab7be457abeab10f7c5ac13f3
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-03-15 15:43:06 -0400

    test for SignUpActivity created

---
+++ Project 8/app/src/androidTest/java/com/example/myapplication/SignUpActivityTest.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class SignUpActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
