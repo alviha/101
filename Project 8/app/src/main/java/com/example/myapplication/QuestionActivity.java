47827051bca461f85966dc0d3b7b316a8089eb24
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-03-22 21:07:12 -0400

    added functionality for setting text to questions and answers

--- Project 8/app/src/main/java/com/example/myapplication/QuestionActivity.java
+++ Project 8/app/src/main/java/com/example/myapplication/QuestionActivity.java
@@ -20,16 +20,48 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
     RadioButton choice3;
     RadioButton choice4;
 
+    private Library.Levels level;
+    private int lesson;
+
+    private String[] questionSet;
+    private String[][] answerChoiceSet;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
 
+        question = findViewById(R.id.text_question);
+        submitAnswer = findViewById(R.id.button_submit);
+        answerChoices = findViewById(R.id.radioGroup_answerChoices);
 
+        submitAnswer.setOnClickListener(this);
+
+        level = Library.Levels.ELEMENTARY_PROGRAMMING; // TODO: dummy object for testing, change to object passed from previous activity
+        lesson = 0; // TODO: dummy object for testing, change to object passed from previous activity
+
+        questionSet = Library.getQuestions(level, lesson);
+        answerChoiceSet = Library.getAnswerChoices(level, lesson);
+
+        question.setText(questionSet[0]);
+
+        for(int i = 0; i < answerChoiceSet[0].length; i++) {
+            ((RadioButton)answerChoices.getChildAt(i)).setText(answerChoiceSet[0][i]);
+        }
     }
 
     @Override
     public void onClick(View v) {
 
+        if(v == submitAnswer) {
+
+//            if(answerIsCorrect()) {
+//
+//            }
+        }
     }
+
+//    private static boolean answerIsCorrect() {
+//
+//    }
 }
