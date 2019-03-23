abe60cf7872d2e42d696827e766998a0468892b8
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-03-22 21:05:54 -0400

    added functionality for lesson button

--- Project 8/app/src/main/java/com/example/myapplication/Homepage.java
+++ Project 8/app/src/main/java/com/example/myapplication/Homepage.java
@@ -1,16 +1,32 @@
 package com.example.myapplication;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class Homepage extends AppCompatActivity {
+public class Homepage extends AppCompatActivity implements View.OnClickListener {
 
+    private Button sample;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        sample = findViewById(R.id.button_ePLesson1);
+        sample.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if ( v == sample){
+            startActivity(new Intent(Homepage.this, LessonActivity.class));
+        }
 
     }
 }
