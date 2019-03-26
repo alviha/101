ddf6aea35cba86921654feb9976833b2e29a19d9
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-03-25 22:13:57 -0400

    added test cases for constructor

--- Project 8/app/src/test/java/com/example/myapplication/AnswerChoiceTest.java
+++ Project 8/app/src/test/java/com/example/myapplication/AnswerChoiceTest.java
@@ -1,29 +1,51 @@
+/**
+ *  Test class of the AnswerChoice class using JUnit 4
+ *
+ * @author Bufford Brian Sta Maria
+ *
+ * CMSC 355, Spring 2019
+ * Project A8
+ * 3/25/2019
+ *
+ */
 package com.example.myapplication;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class AnswerChoiceTest {
 
+    private AnswerChoice testAnswerChoice;
+
+    @Before
+    public void setUp() throws Exception {
+        testAnswerChoice = new AnswerChoice("row", true);
+    }
+
+    // test the constructor: assert the content
     @Test
-    public void AnswerChoice() {
-        
+    public void testAnswerChoice1() {
+        String expected = "1";
+        AnswerChoice testAnswer = new AnswerChoice("1", true);
+        assertEquals(expected, testAnswer.getContent());
+    }
+
+    // test the constructor: assert the isCorrect value
+    @Test
+    public void testAnswerChoice2() {
+        AnswerChoice testAnswer = new AnswerChoice("1", true);
+        assertTrue(testAnswer.isCorrect());
     }
 
     @Test
-    public void setContent() {
+    public void testSetContent() {
+        fail("not yet implemented");
     }
 
     @Test
-    public void setCorrect() {
-    }
-
-    @Test
-    public void getContent() {
-    }
-
-    @Test
-    public void isCorrect() {
+    public void testSetCorrect() {
+        fail("not yet implemented");
     }
 }
