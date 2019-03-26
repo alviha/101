e52a4181ade7630a000157c82231e2543ef50288
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-03-25 22:33:29 -0400

    implemented the constructor

--- Project 8/app/src/main/java/com/example/myapplication/AnswerChoice.java
+++ Project 8/app/src/main/java/com/example/myapplication/AnswerChoice.java
@@ -5,11 +5,18 @@ public final class AnswerChoice {
     private String content;
     private boolean isCorrect;
 
-    public AnswerChoice(String content, boolean isCorrect);
+    public AnswerChoice(String content, boolean value) {
+        this.content = content;
+        this.isCorrect = value;
+    }
 
-    public void setContent(String content);
+    public void setContent(String content) {
 
-    public void setCorrect(boolean value);
+    }
+
+    public void setCorrect(boolean value) {
+
+    }
 
     public String getContent() {
         return this.content;
