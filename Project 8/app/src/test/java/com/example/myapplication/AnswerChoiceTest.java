1da3fb27cb9a526fb4fa2b97d793cbb578181dfe
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-03-25 21:56:00 -0400

    created test class for AnswerChoice

---
+++ Project 8/app/src/test/java/com/example/myapplication/AnswerChoiceTest.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AnswerChoiceTest {
+
+    @Test
+    public void AnswerChoice() {
+        
+    }
+
+    @Test
+    public void setContent() {
+    }
+
+    @Test
+    public void setCorrect() {
+    }
+
+    @Test
+    public void getContent() {
+    }
+
+    @Test
+    public void isCorrect() {
+    }
+}
