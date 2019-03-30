d8d257e07fd3a889ed71987b23fcaf7590305323
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-03-30 16:55:39 -0400

    LibraryTest created

---
+++ Project 8/app/src/test/java/com/example/myapplication/LibraryTest.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+
+    @Test
+    public void getQuestions() {
+    }
+
+    @Test
+    public void getAnswerChoices() {
+    }
+
+    @Test
+    public void getHints() {
+    }
+}
