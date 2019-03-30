51d8c5ab24ae6b468f7b43fca4c0c71e9d18104a
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-03-30 18:36:46 -0400

    test cases for Homepage added

--- Project 8/app/src/androidTest/java/com/example/myapplication/HomepageTest.java
+++ Project 8/app/src/androidTest/java/com/example/myapplication/HomepageTest.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 
@@ -29,7 +30,184 @@ public class HomepageTest {
     @Test
     public void testElementaryProgrammingLesson1() throws InterruptedException {
 
+        Espresso.onView(withId(R.id.button_ePLesson1)).perform(ViewActions.scrollTo());
         Espresso.onView(withId(R.id.button_ePLesson1)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testElementaryProgrammingLesson2() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_ePLesson2)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_ePLesson2)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testElementaryProgrammingLesson3() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_ePLesson3)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_ePLesson3)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testSelectionsLesson1() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_sLesson1)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_sLesson1)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testSelectionsLesson2() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_sLesson2)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_sLesson2)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testSelectionsLesson3() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_sLesson3)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_sLesson3)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testFunctionsCharactersStringsLesson1() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_fCSLesson1)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_fCSLesson1)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testFunctionsCharactersStringsLesson2() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_fCSLesson2)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_fCSLesson2)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testFunctionsCharactersStringsLesson3() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_fCSLesson3)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_fCSLesson3)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testFunctionsCharactersStringsLesson4() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_fCSLesson4)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_fCSLesson4)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testLoopsLesson1() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_while)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_while)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testLoopsLesson2() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_for)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_for)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testLoopsLesson3() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_do)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_do)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testMethodsLesson1() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_mLesson1)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_mLesson1)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testMethodsLesson2() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_mLesson2)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_mLesson2)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testSingleDimensionalArrays1() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_aLesson1)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_aLesson1)).perform(click()); // click the next button
+
+        Thread.sleep(500); // idle test to wait for activity to launch before assertion
+        intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
+    }
+
+    // test: clicking lesson button takes you to the LessonActivity
+    @Test
+    public void testSingleDimensionalArrays2() throws InterruptedException {
+
+        Espresso.onView(withId(R.id.button_aLesson2)).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(R.id.button_aLesson2)).perform(click()); // click the next button
 
         Thread.sleep(500); // idle test to wait for activity to launch before assertion
         intended(hasComponent(LessonActivity.class.getName())); // assert if lesson activity is launched
