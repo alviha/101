c9342a34ba5d2461079c48c7121e87ab800e8ed6
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-03-30 21:30:27 -0400

    added test case for incorrect answer

--- Project 8/app/src/androidTest/java/com/example/myapplication/QuestionActivityTest.java
+++ Project 8/app/src/androidTest/java/com/example/myapplication/QuestionActivityTest.java
@@ -13,15 +13,16 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
 
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.matcher.ViewMatchers.hasTextColor;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
 public class QuestionActivityTest {
@@ -30,16 +31,20 @@ public class QuestionActivityTest {
     public ActivityTestRule<QuestionActivity> mActivityTestRule = new ActivityTestRule<QuestionActivity>(QuestionActivity.class);
 
     private CharSequence correctAnswer;
+    private CharSequence[] incorrectAnswers;
     private AnswerChoice[][] answerChoiceSet;
     RadioGroup answerChoicesGroup;
     int idOfCorrectChoice;
+    int[] idOfIncorrectChoices;
 
 
     @Before
     public void setUp() throws Exception {
 
         correctAnswer = "";
+        incorrectAnswers = new CharSequence[3];
         idOfCorrectChoice = 0;
+        idOfIncorrectChoices = new int[3];
 
         // QuestionActivity is defaulted to launch Lesson 1 of Single Dimensional Arrays
         answerChoiceSet = Library.getAnswerChoices(Library.Levels.SINGLE_DIMENSIONAL_ARRAYS, 0);
@@ -58,19 +63,36 @@ public class QuestionActivityTest {
         Espresso.onView(withId(R.id.text_hint)).check(ViewAssertions.matches(isDisplayed())); // check if hint text is displayed
     }
 
-    // test: submitting the correct answer turns the answer choice green
+    // test: submitting the correct answer displays the next question button
     @Test
     public void testCorrectAnswer() {
 
-        findCorrectAnswer(0);
-        findIdOfCorrectRadioButton();
+        findCorrectAnswerAndIncorrectAnswers(0);
+        findIdOfCorrectAndIncorrectRadioButtons();
 
         Espresso.onView(withId(idOfCorrectChoice)).perform(ViewActions.scrollTo());
         Espresso.onView(withId(idOfCorrectChoice)).perform(click()); // click the correct answer choice
         Espresso.onView(withId(R.id.button_submitAnswer)).perform(click()); // click the submit answer button
 
         Espresso.onView(withId(R.id.button_nextQuestion)).check(ViewAssertions.matches(isDisplayed()));
+    }
 
+    // test: submitting the incorrect answer displays a toast message
+    @Test
+    public void testIncorrectAnswer() throws InterruptedException {
+
+        findCorrectAnswerAndIncorrectAnswers(0);
+        findIdOfCorrectAndIncorrectRadioButtons();
+
+        Espresso.onView(withId(idOfIncorrectChoices[0])).perform(ViewActions.scrollTo());
+        Espresso.onView(withId(idOfIncorrectChoices[0])).perform(click()); // click the correct answer choice
+        Espresso.onView(withId(R.id.button_submitAnswer)).perform(click()); // click the submit answer button
+
+        // check if toast message is displayed
+        Thread.sleep(500); // idle before assertion
+        Espresso.onView(withText("Incorrect"))
+                .inRoot(withDecorView(not(mActivityTestRule.getActivity().getWindow().getDecorView())))
+                .check(matches(isDisplayed()));
     }
 
     @After
@@ -78,21 +100,31 @@ public class QuestionActivityTest {
         Intents.release();
     }
 
-    private void findCorrectAnswer(int questionNumber) {
+    private void findCorrectAnswerAndIncorrectAnswers(int questionNumber) {
 
+        int counter = 0;
         for(AnswerChoice a : answerChoiceSet[questionNumber]) {
             if(a.isCorrect()){
                 correctAnswer = a.getContent();
             }
+            else{
+                incorrectAnswers[counter] = a.getContent();
+                counter++;
+            }
         }
     }
 
-    private void findIdOfCorrectRadioButton() {
+    private void findIdOfCorrectAndIncorrectRadioButtons() {
 
+        int counter = 0;
         for(int i = 0; i < answerChoicesGroup.getChildCount(); i++) {
             RadioButton rb = (RadioButton)answerChoicesGroup.getChildAt(i);
             if(rb.getText().equals(correctAnswer)){
                 idOfCorrectChoice = rb.getId();
+            }
+            else{
+                idOfIncorrectChoices[counter] = rb.getId();
+                counter++;
             }
         }
     }
