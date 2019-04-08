043b778afdaf302d26c898291cda429ecba22bd3
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-04-08 10:58:34 -0400

    adjusted layout, hide profile main buttons when achievements is clicked

--- Project 8/app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ Project 8/app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,13 +1,59 @@
 package com.example.myapplication;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class ProfileActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView header;
+    Button progress, achievements, reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        header = findViewById(R.id.text_ProfileHeader);
+
+        progress = findViewById(R.id.button_Progress);
+        progress.setOnClickListener(this);
+        achievements = findViewById(R.id.button_Achievements);
+        achievements.setOnClickListener(this);
+        reset = findViewById(R.id.button_Reset);
+        reset.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == progress) {
+            // TODO
+        }
+        else if(v == achievements) {
+            hideProfileMainOptions();
+            showAchievementsScreen();
+        }
+        else if(v == reset) {
+            // TODO
+        }
+    }
+
+    private void showAchievementsScreen() {
+
+        header.setText("Achievements");
+        header.setTypeface(Typeface.DEFAULT);
+    }
+
+    private void hideProfileMainOptions() {
+
+        progress.setVisibility(View.GONE);
+        achievements.setVisibility(View.GONE);
+        reset.setVisibility(View.GONE);
     }
 }
