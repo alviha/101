b8c795b63a427e79116e2b21ab75af561de96003
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-04-08 21:58:39 -0400

    code added for saving the image

--- Project 8/app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ Project 8/app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -1,13 +1,22 @@
 package com.example.myapplication;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -43,6 +52,26 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         else if(v == reset) {
             // TODO
         }
+        else if(v == findViewById(R.id.image_certificateOfCompletion)) {
+            Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.certificate_of_completion);
+
+            // path to downloads directory
+            File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            File file = new File(path, "certificate_of_completion.png");
+
+            // save image to downloads directory
+            OutputStream outputStream = null;
+            try {
+                outputStream = new FileOutputStream(file);
+                image.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                outputStream.flush();
+                outputStream.close();
+                Toast.makeText(ProfileActivity.this, "Saved successfully", Toast.LENGTH_SHORT);
+            } catch (java.io.IOException e) {
+                Toast.makeText(ProfileActivity.this, "Saved failed. Please try again.", Toast.LENGTH_SHORT);
+                return;
+            }
+        }
     }
 
     @Override
@@ -71,6 +100,10 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         header.setTypeface(Typeface.DEFAULT);
 
         findViewById(R.id.image_certificateOfCompletion).setVisibility(View.VISIBLE);
+        findViewById(R.id.image_certificateOfCompletion).setOnClickListener(this);
+        findViewById(R.id.text_saveImagePrompt).setVisibility(View.VISIBLE);
+
+        // save image code in OnClick()
     }
 
     private void showAchievementsScreen() {
