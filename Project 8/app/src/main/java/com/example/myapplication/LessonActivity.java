638aeec0bdbfe51572403d40846a16968ea68b2c
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-04-11 19:11:47 -0400

    added resource link functionality

--- Project 8/app/src/main/java/com/example/myapplication/LessonActivity.java
+++ Project 8/app/src/main/java/com/example/myapplication/LessonActivity.java
@@ -2,6 +2,8 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,14 +15,16 @@ public class LessonActivity extends AppCompatActivity implements View.OnClickLis
     // UI elements
     Button toNextQuestion, toHomepage;
 
-    TextView nameOfLesson, intro, body;
+    TextView nameOfLesson, intro, body, link;
+
+    String video;
 
     private String[] lessonSet;
-
 
     // level and lesson data
     private Library.Levels level;
     private int lesson;
+
     private SharedPreferences myPref;
 
     @Override
@@ -35,20 +39,29 @@ public class LessonActivity extends AppCompatActivity implements View.OnClickLis
         intro = findViewById(R.id.text_introduction);
         toNextQuestion = findViewById(R.id.button_toNextQuestion);
         toHomepage = findViewById(R.id.button_toHomepage);
+        link = findViewById(R.id.text_resourceLink);
 
-        //Set listener for buttons
+        //Set listener for buttons and link
         toNextQuestion.setOnClickListener(this);
         toHomepage.setOnClickListener(this);
+        link.setOnClickListener(this);
 
         // retrieve lesson and level data from previous activity
         level = (Library.Levels) getIntent().getSerializableExtra("LEVEL");
         lesson = getIntent().getIntExtra("LESSON", 0);
         //retrieve preferences
         myPref = getSharedPreferences("PREFERENCE", MODE_PRIVATE);
-        
+
         lessonSet = Library.getSampleLessons(level, lesson);
 
         setLessonText();
+
+        // underline text
+        link.setPaintFlags(link.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        // set text to link
+        video = getVideoLink();
+        link.setText(getResources().getString(R.string.helpfulResource));
 
         //finds out if current level is unlocked
         if (myPref.getBoolean(Progress.unlockName(lesson, level), false)) {
@@ -61,29 +74,95 @@ public class LessonActivity extends AppCompatActivity implements View.OnClickLis
     }
 
 
-        @Override
-        public void onClick(View v){
+    @Override
+    public void onClick(View v) {
 
-            // next button is clicked
-            if (v == toNextQuestion) {
+        // next button is clicked
+        if (v == toNextQuestion) {
 
-                // store level and lesson data in the intent
-                Intent intent = new Intent(LessonActivity.this, QuestionActivity.class);
-                intent.putExtra("LEVEL", level);
-                intent.putExtra("LESSON", lesson);
+            // store level and lesson data in the intent
+            Intent intent = new Intent(LessonActivity.this, QuestionActivity.class);
+            intent.putExtra("LEVEL", level);
+            intent.putExtra("LESSON", lesson);
 
-                startActivity(intent);
-                finish();
-            }
-            if (v == toHomepage) {
-                finish();
-            }
+            startActivity(intent);
+            finish();
         }
+        if (v == toHomepage) {
+            finish();
+        }
+        if (v == link) {
 
-        private void setLessonText(){
-
-            nameOfLesson.setText(lessonSet[0]);
-            intro.setText(lessonSet[1]);
-            body.setText(lessonSet[2]);
+            // redirect to youtube video
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(video)));
         }
     }
+
+    private void setLessonText() {
+
+        nameOfLesson.setText(lessonSet[0]);
+        intro.setText(lessonSet[1]);
+        body.setText(lessonSet[2]);
+    }
+
+    private String getVideoLink() {
+
+        String[][] videoLinks = {
+
+                // Elementary Programming Links
+                {
+                        "https://www.youtube.com/watch?v=JpoI_rdMgLM",
+                        "https://www.youtube.com/watch?v=MSiCt6nO2G4",
+                        "https://www.youtube.com/watch?v=sepAXU0V9jk"
+                },
+
+                // Selections Links
+                {
+                        "https://www.youtube.com/watch?v=JpoI_rdMgLM",
+                        "https://www.youtube.com/watch?v=MSiCt6nO2G4",
+                        "https://www.youtube.com/watch?v=sepAXU0V9jk"
+                },
+
+                // Mathematical Functions Characters and Strings Links
+                {
+                        "https://www.youtube.com/watch?v=JpoI_rdMgLM",
+                        "https://www.youtube.com/watch?v=MSiCt6nO2G4",
+                        "https://www.youtube.com/watch?v=sepAXU0V9jk",
+                        "https://www.youtube.com/watch?v=W1GEMdHnCQE"
+                },
+
+                // Loops Links
+                {
+                        "https://www.youtube.com/watch?v=JpoI_rdMgLM",
+                        "https://www.youtube.com/watch?v=MSiCt6nO2G4",
+                        "https://www.youtube.com/watch?v=sepAXU0V9jk"
+                },
+
+                // Methods Links
+                {
+                        "https://www.youtube.com/watch?v=JpoI_rdMgLM",
+                        "https://www.youtube.com/watch?v=MSiCt6nO2G4"
+                },
+
+                // Single-Dimensional Arrays Links
+                {
+                        "https://www.youtube.com/watch?v=JpoI_rdMgLM",
+                        "https://www.youtube.com/watch?v=MSiCt6nO2G4"
+                }
+        };
+
+        if (level == Library.Levels.ELEMENTARY_PROGRAMMING) {
+            return videoLinks[0][lesson];
+        } else if (level == Library.Levels.SELECTIONS) {
+            return videoLinks[1][lesson];
+        } else if (level == Library.Levels.FUNCTIONS_CHARACTERS_STRINGS) {
+            return videoLinks[2][lesson];
+        } else if (level == Library.Levels.LOOPS) {
+            return videoLinks[3][lesson];
+        } else if (level == Library.Levels.METHODS) {
+            return videoLinks[4][lesson];
+        } else {
+            return videoLinks[5][lesson];
+        }
+    }
+}
