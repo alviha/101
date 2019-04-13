590a4562528e707792968586adc4a022c844f625
Bufford Brian Sta Maria <iambufford99@gmail.com>
2019-04-13 12:35:30 -0400

    ProfileActivityTest created

---
+++ Project 8/app/src/androidTest/java/com/example/myapplication/ProfileActivityTest.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProfileActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onBackPressed() {
+    }
+}
